d73a02d5fa2c36974325781331a9edd32e3e3048
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-07-12 14:34:59 +0400

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,13 @@
+name = "vako"
+
+surname = "babajanshvili"
+
+livingplace = "rustavi"
+
+age = "16 years old"
+
+school = "rustavi 18th school"
+
+print(name +" "+ surname +" "+ age)
+
+print(school + " "+ age)
